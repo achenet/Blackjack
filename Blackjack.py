32a7d7ceeeb966dc30323192593c91ebf538f20b
Ariel <apchenet@gmail.com>
2020-09-22 15:46:42 +0200

    working on the main game loop

--- Blackjack.py
+++ Blackjack.py
@@ -73,6 +73,61 @@ class Hand:
         print("Value " + str(self.getValue()))
 
 
+class Game:
+    
+    def __init__(self):
+        pass
+
+    def play(self):
+
+        playing = True
+
+        while playing:
+            self.deck = Deck()
+            self.deck.shuffle()
+
+            self.player_hand = Hand()
+            self.dealer_hard = Hand(dealer=True)
+
+            for i in range(2):
+                self.player_hand.addCard(self.deck.deal())
+                self.dealer_hand.addCard(self.deck.deal())
+
+            print("your hand is ")
+            self.player_hand.display()
+            print("dealer hand is ")
+            self.dealer_hand.display()
+
+            game_over = False
+            while not game_over:
+                player_has_blackjack, dealer_has_blackjack = self.check_for_blackjack()
+                if player_has_blackjack or dealer_has_blackjack:
+                    game_over = True
+                    self.show_blackjack_results(player_has_blackjack,dealer_has_blackjack)
+                    continue
+               choice = input("Please enter Hit, or 'h' if you wish to hit, and Stick, or 's' if you wish to stick").lower()
+               while choice not in ["h","s","hit","stick"]:
+                   choice = input("Please enter 'Hit' or 'Stick' (or H/S)").lower()
+    
+
+
+    def check_for_blackjack(self):
+        player = False
+        dealer = False
+        if self.player_hand.getValue() == 21:
+            player = True
+        if self.dealer_hand.getValue() == 21:
+            dealer = True
+
+        return player,dealer
+
+    def show_blackjack_results(self,player,dealer):
+        if player and dealer:
+            print("Both have blackjack. Draw. ")
+        elif player:
+            print("You have blackjack! A winner is you!")
+        elif dealer:
+            print("Dealer has blackjack! Dealer wins. ")
 
 
 
