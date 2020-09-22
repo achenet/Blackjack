ee2c12f52415c12a1ba4890e0cbe7ce637d7e67a
Ariel <apchenet@gmail.com>
2020-09-22 14:09:51 +0200

    added gitignore

--- Blackjack.py
+++ Blackjack.py
@@ -9,10 +9,31 @@ class Card:
         self.value = value
         self.suit = suit
 
-    def __repr__(self,):
+    def __repr__(self):
         return " of ".join((self.value, self.suit))
 
 
+class Deck:
 
-card = Card("Q","Hearts")
-print(card)
+    def __init__(self):
+        self.cards = [Card(v,s) for v in ["A","2","3","4","5","6","7","8","9","10","J","Q","K"] for s in ["Clubs","Diamonds","Hearts","Spades"]]
+
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
+
+    def deal(self):
+        if len(self.cards) > 1:
+            return self.cards.pop(0)
+
+    def __repr__(self):
+        return "The deck has " + str(len(self.cards)) + " cards"
+
+
+deck = Deck()
+print(deck)
+deck.shuffle()
+for i in deck.cards:
+    print(i)
+
+
